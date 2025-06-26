9250d51fc74929435d0eb069037ef535dacc58b9
chrgarcia19 <christian.garcia@valpo.edu>
2025-06-26 16:19:35 -0500

    Moved the homepage code to its own directory and updated routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,15 @@
 import ProjectCard from "@/components/ProjectCard";
 import paintImage from "../../assets/PaintTestImage.png";
-import christianImage from "../../assets/ChristianGarciaPhoto.jpg";
 import NavBar from "@/components/NavBar";
 import SectionHeader from "@/components/SectionHeader";
-import Image from "next/image";
-import StyledButton from "@/components/StyledButton";
 
-import { FaGithubSquare, FaLinkedin } from "react-icons/fa";
-import { SiIndeed } from "react-icons/si";
-import { IoIosMail } from "react-icons/io";
+import Homepage from "./(home)/page";
 
 export default function Home() {
   return (
     <>
       <NavBar/>
-      <div className="flex pt-14 h-screen w-full">
-        <div className="flex items-center justify-center w-1/4 pl-6">
-          <div className="border border-black rounded-3xl overflow-hidden">
-            <Image 
-              src={christianImage} 
-              alt={"An Image of Christian"}
-              width={1800} 
-            />
-          </div>
-        </div>
-        <div className="flex flex-col items-center justify-center w-3/4">
-          <div className="flex justify-center items-center gap-x-16 p-10">
-            <div className="justify-center space-y-2">
-              <p className="flex font-extrabold text-7xl justify-center">Hi! I'm Christian Garcia</p>
-              <br/>
-              <p>An aspiring Software Engineer passionate about creating and contributing to software that makes a meaningful impact on users.</p>
-            </div>
-          </div>
-          <div className="flex gap-8">
-            <StyledButton href={"#projects"} text={"View Projects"} color={"bg-sky-600"} hoveredColor={"bg-sky-500"} icon={undefined}/>
-            <StyledButton href={""} text={"Download Resume"} color={"bg-sky-600"} hoveredColor={"bg-sky-500"} icon={undefined}/>
-          </div>
-          <div className="flex gap-4 pt-12">
-              <StyledButton href={"https://www.linkedin.com/in/chrgarcia19/"} text={""} color={"bg-sky-500"} hoveredColor={"bg-sky-600"} icon={<FaLinkedin size={36}/>} />
-              <StyledButton href={"https://profile.indeed.com/p/christiang-mg6ntmr"} text={""} color={"bg-sky-500"} hoveredColor={"bg-sky-600"} icon={<SiIndeed size={36}/>} />
-              <StyledButton href={"https://github.com/chrgarcia19"} text={""} color={"bg-sky-500"} hoveredColor={"bg-sky-600"} icon={<FaGithubSquare size={36}/>} />
-              <StyledButton href={"mailto:christian.garcia@valpo.edu"} text={""} color={"bg-sky-500"} hoveredColor={"bg-sky-600"} icon={<IoIosMail size={36}/>} />
-          </div>
-        </div>
-      </div>
+      <Homepage />
   
       <div id="about" className="flex items-center justify-center p-5 w-full h-screen">
         <h2 className="flex items-start justify-start font-bold text-3xl p-6">About Me</h2>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,7 +3,7 @@ import StyledButton from "./StyledButton";
 const NavBar = () => {
 
     const NAV_ITEMS = [
-        { text: "Home", href: "" },
+        { text: "Home", href: "/" },
         { text: "About", href: "#about" },
         { text: "Skills", href: "#skills" },
         { text: "Projects", href: "#projects" }, 
@@ -15,7 +15,7 @@ const NavBar = () => {
         <>
             <nav className="flex w-full p-2 items-center justify-between bg-sky-600 overflow-hidden fixed z-10">
                 <div className="flex justify-start">
-                    <StyledButton href={""} text={"Christian Garcia"} color={"bg-sky-600"} hoveredColor={"bg-sky-500"} icon={undefined}/>
+                    <StyledButton href={"/"} text={"Christian Garcia"} color={"bg-sky-600"} hoveredColor={"bg-sky-500"} icon={undefined}/>
                 </div>    
                 <div className="flex space-x-12 mr-10">
                     {NAV_ITEMS.map((item: any, index: number) => (
