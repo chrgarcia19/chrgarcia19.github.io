8b061d0c7b36199625c10a61f212a5ef041cc66d
chrgarcia19 <christian.garcia@valpo.edu>
2025-07-14 15:32:01 -0500

    Reorganized the project card layout

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -7,11 +7,12 @@ const Projects = () => {
             <div id="projects" className="p-5 pt-14 w-full h-screen">
                 <h2 className="flex justify-center items-center font-bold text-3xl p-10">Projects</h2>
                 <ProjectCard 
-                    name={"Paint"} 
-                    description={"This project required me to create my own representation of a drawing and editing application with similar capabilities to Microsoft Paint using Java and JavaFX."} 
-                    tools={["Java", "JavaFX", "Agile Method of Software Development", "Unit Testing", "Netbeans", "IntelliJ", "JUnit", "Javadoc", "Unified Modeling Language (UML)"]} 
-                    image={paintImage} imageAlt={"A Screenshot from Christian's Paint project"} 
-                    link={"/paint"} />
+                    name={"Paint"}
+                    description={"This project required me to create my own representation of a drawing and editing application with similar capabilities to Microsoft Paint using Java and JavaFX."}
+                    tools={["Java", "JavaFX", "Unit Testing", "Netbeans", "IntelliJ", "JUnit", "Javadoc"]}
+                    image={paintImage} imageAlt={"A Screenshot from Christian's Paint project"}
+                    link={"https://github.com/chrgarcia19/Paint-n-Suffering"} 
+                    concepts={["Agile Method of Software Development", "Unified Modeling Language (UML)"]} />
             </div>
         </>
     );
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,6 +1,5 @@
 import Image, { StaticImageData } from "next/image";
-import Link from "next/link";
-import HoverableImage from "./HoverableImage";
+import StyledButton from "./StyledButton";
 
 type Props = {
     link: string; // The url for the project page
@@ -9,27 +8,38 @@ type Props = {
     name: String; // The name of the project
     description: String; // The description of the project
     tools: String[]; // The tools used to create the project
+    concepts: String[]; //The concepts used when creating the project
 };
 
 const ProjectCard = (props: Props) => {
     return (
         <>
-            <div className="flex justify-center">
-                <div className="project-card">
-                    <HoverableImage link={props.link} image={props.image} imageAlt={props.imageAlt} />
-                    <p className="font-bold text-2xl">{props.name}</p>
+            <div className="flex flex-col rounded-2xl w-5/12 bg-cyan-300">
+                <div className="w-full h-full overflow-hidden rounded-t-2xl">
+                    <Image className={"w-full h-auto object-cover rounded-t-2xl"} src={props.image} alt={props.imageAlt} priority/>
+                </div>
+                <div className="flex flex-col p-4 w-full">
+                    <p className="font-bold text-xl">{props.name}</p>
+                    <p>{props.description}</p>
                     <span className="p-1">
-                        <p className="font-bold underline">Description: </p>
-                        <p>{props.description}</p>
-                    </span>
+                            <p className="font-bold underline">Technologies Used: </p>
+                            <div className="flex flex-wrap gap-3">
+                                {props.tools.map((tool: String, index: number) => (
+                                        <p key={index} className="rounded-full bg-green-400 px-3 shadow-md">{tool}</p>
+                                ))}
+                            </div>
+                        </span>
                     <span className="p-1">
-                        <p className="font-bold underline">Skills Used: </p>
+                        <p className="font-bold underline">Concepts Used: </p>
                         <div className="flex flex-wrap gap-3">
-                            {props.tools.map((tool: String, index: number) => (
-                                    <p key={index} className="rounded-full bg-green-400 px-3 shadow-md">{tool}</p>
+                            {props.concepts.map((concept: String, index: number) => (
+                                    <p key={index} className="rounded-full bg-green-400 px-3 shadow-md">{concept}</p>
                             ))}
                         </div>
                     </span>
+                    <div className="flex p-2">
+                        <StyledButton href={props.link} text={"View Project"} color={"bg-green-500"} hoveredColor={"bg-green-200"} icon={undefined} />
+                    </div>
                 </div>
             </div>
         </>
