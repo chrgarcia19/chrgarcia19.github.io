2334a35f07b9ff38e47b427c52fb2a8dc7597ba3
chrgarcia19 <christian.garcia@valpo.edu>
2025-07-17 14:38:01 -0500

    Modified the project card component to clean up the layout

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -7,8 +7,7 @@ type Props = {
     imageAlt: string; // The alternative words if the image cannot be accessed
     name: String; // The name of the project
     description: String; // The description of the project
-    tools: String[]; // The tools used to create the project
-    concepts: String[]; //The concepts used when creating the project
+    skills: String[]; //The skills used on the project
 };
 
 const ProjectCard = (props: Props) => {
@@ -18,33 +17,21 @@ const ProjectCard = (props: Props) => {
                 <div className="relative w-full h-[300px]">
                     <Image className="object-cover object-center" src={props.image} alt={props.imageAlt} fill sizes="100vw" />
                 </div>
-                <div className="flex flex-col p-4 w-full">
-                    <p className="font-bold text-xl">{props.name}</p>
+                <div className="flex flex-col p-6 w-full">
+                    <p className="font-bold text-xl underline">{props.name}</p>
                     <p>{props.description}</p>
                     <span className="p-1">
-                    <p className="font-bold underline">Technologies Used:</p>
-                    <div className="flex flex-wrap gap-3">
-                        {props.tools.map((tool: String, index: number) => (
+                    <p className="font-bold underline">Skills Used:</p>
+                    <div className="flex flex-wrap gap-2">
+                        {props.skills.map((skill: String, index: number) => (
                         <p key={index} className="rounded-full bg-green-400 px-3 shadow-md text-sm">
-                            {tool}
+                            {skill}
                         </p>
                         ))}
                     </div>
                     </span>
-
-                    <span className="p-1">
-                    <p className="font-bold underline">Concepts Used:</p>
-                    <div className="flex flex-wrap gap-3">
-                        {props.concepts.map((concept: String, index: number) => (
-                        <p key={index} className="rounded-full bg-green-400 px-3 shadow-md text-sm">
-                            {concept}
-                        </p>
-                        ))}
-                    </div>
-                    </span>
-
                     <div className="flex p-2">
-                    <StyledButton href={props.link} text={"View Project"} color={"bg-green-500"} hoveredColor={"bg-green-200"} icon={undefined} />
+                        <StyledButton href={props.link} text={"View Project"} color={"bg-green-500"} hoveredColor={"bg-green-200"} icon={undefined} other={"w-full text-center"} />
                     </div>
                 </div>
             </div>
