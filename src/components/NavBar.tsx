d86517bbf85a248dd80abb60eaa9872d815fb1ee
chrgarcia19 <christian.garcia@valpo.edu>
2025-07-22 13:03:03 -0500

    Removed the hyperlink from my name in the navbar to remove redundant functions

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,5 @@
 import DownloadButton from "./DownloadButton";
+import ItemBox from "./ItemBox";
 import StyledButton from "./StyledButton";
 
 const NavBar = () => {
@@ -15,7 +16,9 @@ const NavBar = () => {
         <>
             <nav className="flex w-full p-2 items-center justify-between bg-cyan-500 overflow-hidden fixed z-10">
                 <div className="flex justify-start">
-                    <StyledButton href={"/"} text={"Christian Garcia"} color={"bg-cyan-500"} hoveredColor={"bg-cyan-600"} icon={undefined} other={undefined}/>
+                    <ItemBox color={"bg-cyan-500"} hoveredColor={"bg-cyan-600"} width={undefined} boxType={"rounded-full"} padding={"px-6"} other={undefined}>
+                        <h1 className="text-3xl text-white font-bold">Christian Garcia</h1> 
+                    </ItemBox>
                 </div>    
                 <div className="flex space-x-4 mr-10">
                     {NAV_ITEMS.map((item: any, index: number) => (
