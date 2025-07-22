d28f30301341b0c790d5a92ff890f4dfbd694750
chrgarcia19 <christian.garcia@valpo.edu>
2025-07-22 13:08:33 -0500

    Changed some text on the contact section

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -8,7 +8,7 @@ const Contact = () => {
             <div id="contact" className="flex flex-col pt-10 w-full bg-zinc-300">
                 <div className="flex flex-col items-center justify-center bg-blue-950 gap-6 p-10">
                     <h3 className="font-bold text-5xl p-10 text-zinc-300">Get In Touch</h3>
-                    <p className="text-2xl text-center w-1/2 p-5 text-white">I am actively seeking new opportunities in software development. If you have a role or project that aligns with my skills and experience, I would welcome the chance to connect. Please feel free to reach out!</p>
+                    <p className="text-2xl text-center w-1/2 p-5 text-white">I am actively seeking new opportunities in software engineering. If you have a role or project that aligns with my skills and experience, I would welcome the chance to connect. Please feel free to reach out!</p>
                     <StyledButton href={"mailto:christian.garcia@valpo.edu"} text={"Connect With Christian"} color={"bg-cyan-500"} hoveredColor={"bg-cyan-600"} icon={undefined} other={"py-6 text-xl"} />
                     <span className="flex flex-row gap-4 p-4">
                         <StyledButton href={"https://www.linkedin.com/in/chrgarcia19/"} text={undefined} color={"bg-cyan-500"} hoveredColor={"bg-cyan-600"} icon={<FaLinkedin size={32} />} other={undefined} />
