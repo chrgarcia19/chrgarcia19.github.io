7a3ff48d89caee677f685051bdb16c0d267548a9
chrgarcia19 <christian.garcia@valpo.edu>
2025-07-30 19:49:01 -0500

    Enhanced styling on the projects section to accommodate on different screen sizes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
       <Homepage />
       <AboutMe />
       <Skills />
-      {/*<Projects />
-      <Contact />*/}
+      <Projects />
+      {/*<Contact />*/}
     </>
   );
 }
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -25,34 +25,36 @@ const ProjectCard = (props: Props) => {
     return (
         <>
             <div onMouseEnter={itemEntered} onMouseLeave={itemExited} 
-                className={`flex flex-col rounded-2xl border-2 border-slate-400 dark:border-slate-700 w-1/3 overflow-hidden ${isHovered ? `shadow-xl shadow-slate-500` : ''}`}>
+                className={`flex flex-col rounded-2xl border-2 border-slate-400 dark:border-slate-700 w-1/3 max-xl:w-5/12 max-md:w-full overflow-hidden ${isHovered ? `shadow-xl shadow-slate-500` : ''}`}>
                 <div className="relative w-full h-[300px]">
                     <Image className="object-cover object-center" src={props.image} alt={props.imageAlt} fill sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" />
                 </div>
                 <div className="flex flex-col p-4 w-full">
-                    <span className="flex flex-row items-center space-x-3">
-                        <p className="font-bold text-2xl underline">{props.name}</p>
-                        {props.status && (
-                            <p className="bg-green-500 dark:bg-green-600 font-semibold w-fit rounded-full px-2 text-sm">Completed</p>
-                        ) || (
-                            <p className="bg-yellow-300 dark:bg-yellow-500 font-semibold w-fit rounded-full px-2 text-sm">In Progress</p>
-                        )}
+                    <span className="flex xl:flex-row max-md:flex-col items-center pb-2 xl:space-x-4">
+                        <p className="font-bold text-2xl max-md:text-xl underline text-center">{props.name}</p>
+                        <div className="p-1">
+                            {props.status && (
+                                <p className="bg-green-500 dark:bg-green-600 font-semibold w-fit rounded-full px-2 max-xl:px-4 max-xl:py-1 text-sm max-md:text-xs text-center">Completed</p>
+                            ) || (
+                                <p className="bg-yellow-300 dark:bg-yellow-500 font-semibold w-fit rounded-full px-2 max-xl:px-4 max-xl:py-1 text-sm max-md:text-xs text-center">In Progress</p>
+                            )}
+                        </div>
                     </span>
-                    <p>{props.description}</p>
-                    <span className="p-1">
-                    <p className="font-bold underline">Skills Used:</p>
-                    <div className="flex flex-wrap gap-2 p-2 m-2 place-content-center">
-                        {props.skills.map((skill: String, index: number) => (
-                            <div key={index}>
-                                <ItemBox color={"bg-blue-800 dark:bg-blue-300"} hoveredColor={"bg-blue-950 dark:bg-blue-600"} width={""} boxType={"rounded-full"} padding={"px-2"} other={"text-white dark:text-black"}>
-                                    {skill}
-                                </ItemBox>
-                            </div>
-                        ))}
-                    </div>
+                    <p className="max-md:text-sm">{props.description}</p>
+                    <span className="pt-2">
+                        <p className="font-bold underline">Skills Used:</p>
+                        <div className="flex flex-wrap gap-2 p-2 m-2 place-content-center text-center">
+                            {props.skills.map((skill: String, index: number) => (
+                                <div key={index}>
+                                    <ItemBox color={"bg-blue-800 dark:bg-blue-300"} hoveredColor={"bg-blue-950 dark:bg-blue-600"} width={""} boxType={"rounded-full"} padding={"px-2"} other={"text-white dark:text-black max-md:text-sm"}>
+                                        {skill}
+                                    </ItemBox>
+                                </div>
+                            ))}
+                        </div>
                     </span>
-                    <div className="flex p-2">
-                        <StyledButton href={props.link} text={"GitHub"} color={"bg-green-500"} hoveredColor={"bg-green-200"} icon={<SiGithub />} other={"w-full text-center"} />
+                    <div className="flex pt-2">
+                        <StyledButton href={props.link} text={"GitHub"} color={"bg-green-500"} hoveredColor={"bg-green-200"} icon={<SiGithub />} other={"w-full text-center"} textSize={""} padding={""} />
                     </div>
                 </div>
             </div>
