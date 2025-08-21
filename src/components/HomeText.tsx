e59be9168432e765d146a889c7b8b54d8dae7cbc
chrgarcia19 <christian.garcia@valpo.edu>
2025-08-21 15:49:31 -0500

    Added a couple of changes to the animations

--- src/components/HomeText.tsx
+++ src/components/HomeText.tsx
@@ -14,6 +14,8 @@ const HomeText = () => {
   const [index, setIndex] = useState(0);
   const [displayText, setDisplayText] = useState("");
   const [charIndex, setCharIndex] = useState(0);
+  const [displayTyping, setDisplayTyping] = useState("_");
+  const [phraseComplete, setPhraseComplete] = useState(false);
 
   useEffect(() => {
     const currentPhrase = phrases[index];
@@ -27,11 +29,15 @@ const HomeText = () => {
       
       return () => clearTimeout(timeout);
     } else {
-    
+      setPhraseComplete(true);
+      setDisplayTyping("");
+
       const timeout = setTimeout(() => {
         setIndex(prev => (prev + 1) % phrases.length);
         setCharIndex(0);
         setDisplayText("");
+        setPhraseComplete(false);
+        setDisplayTyping("_")
       }, 5000);
       
       return () => clearTimeout(timeout);
@@ -41,9 +47,9 @@ const HomeText = () => {
   return (
     <>
       <div className="z-0 flex items-center justify-center text-5xl max-sm:text-2xl 3xl:text-6xl 4k:text-7xl text-center font-semibold min-h-[1.2em]">
-        <span>
+        <span className={phraseComplete ? "animate-pulse" : ""}>
           {displayText}
-          <span className="animate-pulse">_</span>
+          <span className="animate-ping">{phraseComplete ? "" : "_"}</span>
         </span>
       </div>
     </>
