670c471e9231ec912dc8b1de5ba3a8aa01b46e4e
chrgarcia19 <christian.garcia@valpo.edu>
2025-08-26 18:15:38 -0500

    Modified some styling on the homepage

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -15,14 +15,14 @@ const Homepage = () => {
               <p className="flex justify-center text-5xl max-sm:text-4xl 3xl:text-7xl 4k:text-8xl font-semibold drop-shadow-md">
                 Hi! I am
               </p>
-              <h1 className="flex font-extrabold text-7xl max-sm:text-3xl 3xl:text-8xl 4k:text-9xl justify-center text-blue-950 dark:text-blue-300 drop-shadow-xl">
+              <h1 className="flex justify-center text-center font-extrabold text-7xl max-sm:text-3xl max-md:text-6xl 3xl:text-8xl 4k:text-9xl text-blue-950 dark:text-blue-300 drop-shadow-xl">
                 Christian Garcia
               </h1>
               <br />
               <HomeText />
             </div>
           </div>
-          <div className="flex place-content-center gap-8 max-sm:gap-2 4k:gap-10 max-sm:p-4 max-md:p-6 4k:p-8">
+          <div className="flex justify-center items-center gap-8 max-sm:gap-2 4k:gap-10 max-sm:p-4 max-md:p-6 4k:p-8">
             <StyledButton
               href={"#about"}
               text={"More About Me"}
@@ -31,7 +31,7 @@ const Homepage = () => {
               icon={null}
               other={"shadow-lg border-2"}
               textSize={"text-xl max-sm:text-sm 3xl:text-3xl 4k:text-5xl"}
-              padding={"max-sm:px-2 md:px-3 lg:px-4 3xl:px-6 4k:px-10 py-3 max-sm:py-1 4k:py-5"}
+              padding={"max-md:px-2 sm:px-3 lg:px-4 3xl:px-6 4k:px-10 py-3 max-sm:py-1 4k:py-5"}
               label={"Learn More About Christian"}
               title={"Learn More About Christian"}
               textColor={"text-slate-700 dark:text-white"} 
@@ -45,7 +45,7 @@ const Homepage = () => {
               icon={null}
               other={"shadow-xl"}
               textSize={"text-2xl max-sm:text-sm 3xl:text-3xl 4k:text-5xl"}
-              padding={"max-sm:px-2 md:px-3 lg:px-8 3xl:px-6 4k:px-10 py-3 max-sm:py-1 4k:py-5"}
+              padding={"max-md:px-2 sm:px-3 lg:px-8 3xl:px-6 4k:px-10 py-3 max-sm:py-1 4k:py-5"}
               label={"View Projects Section"}
               title={"View Projects Section"}
               textColor={"text-white"} 
@@ -53,7 +53,7 @@ const Homepage = () => {
               />
             <DownloadButton
               text={"View Résumé"}
-              padding={"max-sm:px-2 md:px-3 lg:px-4 3xl:px-6 4k:px-10 py-3 max-sm:py-1 4k:py-5"}
+              padding={"max-sm:px-2 sm:px-3 lg:px-4 3xl:px-6 4k:px-10 py-3 max-sm:py-1 4k:py-5"}
               textSize={"text-xl max-sm:text-sm 3xl:text-3xl 4k:text-5xl"}
               view={true} 
               hovered={"bg-cyan-600 dark:hover:bg-cyan-600 font-bold text-xl shadow-xl"}
