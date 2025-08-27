772bc3e680ee2a2a34e8b781041c015404d10028
chrgarcia19 <christian.garcia@valpo.edu>
2025-08-27 18:58:26 -0500

    Added the gradient border to the right side of the background

--- src/components/SkillSection.tsx
+++ src/components/SkillSection.tsx
@@ -11,12 +11,13 @@ const SkillSection = (props: Props) => {
         <>
             <div className="flex mb-8">
                 <div className={`w-[6px] rounded-l-2xl bg-gradient-to-b ${props.gradientColors}`} />
-                <div className="flex-1 bg-zinc-100/70 dark:bg-zinc-600/70 rounded-r-2xl shadow-md p-6">
+                <div className="flex-1 bg-zinc-100/70 dark:bg-zinc-600/70 shadow-md p-6">
                     <h3 className="flex justify-start max-md:justify-center text-center text-3xl max-md:text-xl 3xl:text-5xl 4k:text-7xl font-bold italic underline p-2">
                         {props.sectionTitle}
                     </h3>
                     {props.skills}
                 </div>
+                <div className={`w-[6px] rounded-r-2xl bg-gradient-to-t ${props.gradientColors}`} />
             </div>
         </>
     );
