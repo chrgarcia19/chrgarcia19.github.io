2eae6197aff24de36c13982f4ab022ea9d5964b7
chrgarcia19 <christian.garcia@valpo.edu>
2025-09-04 18:23:42 -0500

    Changed config files to deploy the website to GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "export",
+    images: {
+        unoptimized: true,
+    },
+    basePath: process.env.NODE_ENV === "production" ? "/christiangarcia.tech" : "",
+    assetPrefix: process.env.NODE_ENV === "production" ? "/christiangarcia.tech" : "",
+};
 
 export default nextConfig;
