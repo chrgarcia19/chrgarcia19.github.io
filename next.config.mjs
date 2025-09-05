a420a23c0c63d877dbfef8dd3909db62ec0fbcfc
chrgarcia19 <christian.garcia@valpo.edu>
2025-09-04 20:00:17 -0500

    Testing to see if I need the assetPrefix and basePath in nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -2,15 +2,15 @@
 
 import process from 'process';
 
-const isProd = process.env.NODE_ENV === "production";
+//const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
     output: "export",
     images: {
         unoptimized: true,
     },
-    basePath: isProd ? "/chrgarcia19.github.io" : "",
-    assetPrefix: isProd ? "/chrgarcia19.github.io" : "",
+    //basePath: isProd ? "/chrgarcia19.github.io" : "",
+    //assetPrefix: isProd ? "/chrgarcia19.github.io" : "",
 };
 
 export default nextConfig;
