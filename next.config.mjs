ecf61ad3eebbbd152c500de508694951de96572f
chrgarcia19 <christian.garcia@valpo.edu>
2025-09-04 20:10:11 -0500

    Removing the lines of code after testing showed that I did not need them

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,10 @@
 /** @type {import('next').NextConfig} */
 
-import process from 'process';
-
-//const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
     output: "export",
     images: {
         unoptimized: true,
     },
-    //basePath: isProd ? "/chrgarcia19.github.io" : "",
-    //assetPrefix: isProd ? "/chrgarcia19.github.io" : "",
 };
 
 export default nextConfig;
